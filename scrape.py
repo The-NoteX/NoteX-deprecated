bf5b692b0ad7132edee90e6a1b4b3736cfd64cf9
TeeWrath <subroto.2003@gmail.com>
2023-08-12 13:26:08 +0530

    scraping script ready

--- scrape.py
+++ scrape.py
@@ -1,11 +1,33 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import json
+import time
 
 def scrape_data():
     driver = webdriver.Firefox()
     driver.get("https://mlh.io/seasons/2024/events")
-    links = driver.find_elements(By.CLASS_NAME, "event-link")
-    for link in links:
-        print(link.get_attribute("href"))
-scrape_data()
+    links = driver.find_elements(By.CLASS_NAME, "event-link") # Fetching links of hackathon websites
+    images = driver.find_elements(By.CSS_SELECTOR, ".image-wrap img") # Fetching images of hackathons
+    names = driver.find_elements(By.CLASS_NAME, "event-name") # Fetching names of hackathons
+    dates = driver.find_elements(By.CLASS_NAME, "event-date") # Fetching dates
+    locs = driver.find_elements(By.CLASS_NAME, "event-location") # Fetching location
+    modes = driver.find_elements(By.CLASS_NAME,"event-hybrid-notes") # Fetching modes
+    hackathons = []
+    for i in range(len(links)):
+        hackathon = {
+            "name": names[i].text,
+            "link": links[i].get_attribute("href"),
+            "image": images[i].get_attribute("src"),
+            "date": dates[i].text,
+            "location": locs[i].text,
+            "mode":modes[i].text
+        }
+        hackathons.append(hackathon)
+    with open("hackathons.json","w") as f:
+        json.dump(hackathons,f,indent=4); 
+    driver.quit()
+
+# scrape_data()
+while(1):
+    scrape_data()
+    time.sleep(86400)
