f92b250608d65ede9164aafb41a3b1b9e9e2d53e
Bhaskar Metiya <bhaskarmetiya@gmail.com>
2023-08-13 09:20:49 +0530

    feat: display data from hackathons api

--- lib/hackathon/explore_hackatho.dart
+++ lib/hackathon/explore_hackatho.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notex/utils/constants.dart';
 
 import 'api_hackathon.dart';
 
@@ -26,21 +27,29 @@ class _ExploreHackathonsState extends State<ExploreHackathons> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('API Data Display'),
+        title: const Text(
+          'Explore Hackathons',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        backgroundColor: backgroundColor,
       ),
       body: Center(
-        child: data.isNotEmpty
-            ? ListView.builder(
-                itemCount: data.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(data[index]['title']),
-                    subtitle: Text(data[index]['description']),
-                  );
-                },
-              )
-            : const CircularProgressIndicator(),
-      ),
+          // for item in data make listTile
+          child: Scrollbar(
+        child: ListView(
+          children: [
+            for (var item in data)
+              ListTile(
+                title: Text(item['name']),
+                subtitle: Text(item['link']),
+              ),
+          ],
+        ),
+      )),
     );
   }
 }
