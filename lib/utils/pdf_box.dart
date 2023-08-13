15de9941547b70564b9046088cd8d3c80bdad1c0
Bhaskar Metiya <bhaskarmetiya@gmail.com>
2023-08-13 11:35:16 +0530

    feat: explore notes ui

--- lib/utils/pdf_box.dart
+++ lib/utils/pdf_box.dart
@@ -18,98 +18,117 @@ class _PdfBoxState extends State<PdfBox> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: const EdgeInsets.all(20.0),
       decoration: BoxDecoration(
-        color: const Color.fromARGB(255, 223, 223, 223),
-        borderRadius: BorderRadius.circular(10),
-      ),
+          color: const Color.fromARGB(255, 223, 223, 223),
+          borderRadius: BorderRadius.circular(20),
+          border: Border.all(
+            width: 2,
+            color: Colors.black,
+          )),
       child: Column(
         children: [
-          Center(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text(
-                  widget.snap['subject'],
-                  style: GoogleFonts.manrope(
-                      fontSize: 30, fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(height: 20),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text(
-                  '${widget.snap['author']}',
-                  style: GoogleFonts.manrope(
-                      fontSize: 25, fontWeight: FontWeight.w600),
-                ),
-                const SizedBox(width: 10),
-                Container(
-                  width: 90,
-                  height: 40,
-                  decoration: BoxDecoration(
-                      color: backgroundColor,
-                      borderRadius: BorderRadius.circular(20),
-                      border: Border.all(width: 1.7)),
-                  child: Center(
-                    child: Text(
-                      widget.snap['tags'],
-                      style: GoogleFonts.manrope(
-                          fontSize: 17, fontWeight: FontWeight.w500),
-                    ),
-                  ),
-                ),
-                Text(
-                  'Semester ${widget.snap['sem']}',
-                  style: GoogleFonts.manrope(
-                      fontSize: 25, fontWeight: FontWeight.w500),
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(height: 15),
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              TextButton(
-                style: const ButtonStyle(
-                    iconSize: MaterialStatePropertyAll(20),
-                    foregroundColor: MaterialStatePropertyAll(Colors.black)),
-                child: const FaIcon(FontAwesomeIcons.shareNodes),
-                onPressed: () {},
-              ),
-              TextButton(
-                style: const ButtonStyle(
-                    iconSize: MaterialStatePropertyAll(20),
-                    foregroundColor: MaterialStatePropertyAll(Colors.black)),
-                child: const FaIcon(FontAwesomeIcons.comment),
-                onPressed: () {},
-              ),
-              TextButton(
-                style: const ButtonStyle(
-                    iconSize: MaterialStatePropertyAll(20),
-                    foregroundColor: MaterialStatePropertyAll(Colors.black)),
-                child: const FaIcon(FontAwesomeIcons.download),
-                onPressed: () {},
-              ),
-              TextButton(
-                style: const ButtonStyle(
-                    iconSize: MaterialStatePropertyAll(20),
-                    foregroundColor: MaterialStatePropertyAll(Colors.black)),
-                child: const FaIcon(FontAwesomeIcons.listCheck),
-                onPressed: () {},
-              ),
+              Text(
+                widget.snap['subject'],
+                style: GoogleFonts.manrope(
+                    fontSize: 25, fontWeight: FontWeight.w600),
+              )
             ],
           ),
-          const SizedBox(height: 12),
-          TextButton(
-            onPressed: () {},
-            child: const Text("Open PDF"),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                widget.snap['author'],
+                style: GoogleFonts.manrope(
+                    fontSize: 17, fontWeight: FontWeight.w500),
+              )
+            ],
           ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    width: 90,
+                    height: 40,
+                    decoration: BoxDecoration(
+                        color: backgroundColor,
+                        borderRadius: BorderRadius.circular(20),
+                        border: Border.all(width: 1.7)),
+                    child: Center(
+                      child: Text(
+                        widget.snap['tags'],
+                        style: GoogleFonts.manrope(
+                            fontSize: 17, fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                      onPressed: () => {},
+                      child: Text(
+                        "Open PDF",
+                        style: GoogleFonts.manrope(
+                            fontSize: 17,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black),
+                      ))
+                ],
+              ),
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 30.0),
+                    child: Text(
+                      'Semester ${widget.snap['sem']}',
+                      style: GoogleFonts.manrope(
+                          fontSize: 25, fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      TextButton(
+                        style: const ButtonStyle(
+                            iconSize: MaterialStatePropertyAll(20),
+                            foregroundColor:
+                                MaterialStatePropertyAll(Colors.black)),
+                        child: SizedBox(width: 20.0,child: const FaIcon(FontAwesomeIcons.shareNodes)),
+                        onPressed: () {},
+                      ),
+                      TextButton(
+                        style: const ButtonStyle(
+                            iconSize: MaterialStatePropertyAll(20),
+                            foregroundColor:
+                                MaterialStatePropertyAll(Colors.black)),
+                        child: const FaIcon(FontAwesomeIcons.comment),
+                        onPressed: () {},
+                      ),
+                      TextButton(
+                        style: const ButtonStyle(
+                            iconSize: MaterialStatePropertyAll(20),
+                            foregroundColor:
+                                MaterialStatePropertyAll(Colors.black)),
+                        child: const FaIcon(FontAwesomeIcons.download),
+                        onPressed: () {},
+                      ),
+                      TextButton(
+                        child: Icon(Icons.thumb_up),
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+                ],
+              )
+            ],
+          )
         ],
       ),
     );
