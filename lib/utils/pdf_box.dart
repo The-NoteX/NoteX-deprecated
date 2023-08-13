bc91384d89a55b7bb4e9606432c75e2c212b9323
RJ-04 <riyanshjain2004@gmail.com>
2023-08-13 07:52:21 +0530

    explore notes

--- lib/utils/pdf_box.dart
+++ lib/utils/pdf_box.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:notex/utils/constants.dart';
 
 class PdfBox extends StatefulWidget {
   final snap;
@@ -19,19 +20,53 @@ class _PdfBoxState extends State<PdfBox> {
       padding: const EdgeInsets.all(15),
       decoration: BoxDecoration(
         color: const Color.fromARGB(255, 223, 223, 223),
-        borderRadius: BorderRadius.circular(50),
+        borderRadius: BorderRadius.circular(20),
       ),
-      child: Row(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
             widget.snap['subject'],
             style: GoogleFonts.manrope(
-              fontSize: 20,
+              fontSize: 30,
               fontWeight: FontWeight.bold,
             ),
           ),
           Column(
-            children: [Text(widget.snap[''])],
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '-${widget.snap['author']}',
+                style: GoogleFonts.manrope(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              const SizedBox(height: 30),
+              Container(
+                height: 40,
+                width: 80,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  color: backgroundColor,
+                  border: Border.all(
+                    width: 1.5,
+                  ),
+                ),
+                child: Center(
+                    child: Text(
+                  widget.snap['tags'],
+                  style: GoogleFonts.manrope(
+                    fontSize: 17,
+                    fontWeight: FontWeight.w700,
+                  ),
+                )),
+              ),
+              const SizedBox(height: 30),
+              Container(
+                child: const Text("Open PDF"),
+              ),
+            ],
           ),
         ],
       ),
