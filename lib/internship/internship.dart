21be741e2259113fe58ef2f388938329648aa42c
shashwatdwi176 <dwivedishashwat176@gmail.com>
2023-08-13 11:37:50 +0530

    Added Internship page

--- lib/internship/internship.dart
+++ lib/internship/internship.dart
@@ -1,76 +1,89 @@
-
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
-import '../utils/constants.dart'; // Import the url_launcher package
-
+import '../utils/constants.dart';
 
 class InternshipPage extends StatelessWidget {
-
-
-
-  final List<InternshipWebsite> internshipWebsites = [
-    InternshipWebsite("Internshala", "https://internshala.com/"),
-    InternshipWebsite("LinkedIn", "https://www.linkedin.com"),
-    InternshipWebsite("Indeed", "https://www.indeed.com"),
-    
-  ];
+  PreferredSize appBar = PreferredSize(
+    preferredSize: const Size.fromHeight(56.5),
+    child: AppBar(
+      title: Container(
+        alignment: const Alignment(-0.2, -0.9),
+      ),
+      toolbarHeight: 56.5,
+      backgroundColor: backgroundColor,
+      shadowColor: Colors.transparent,
+    ),
+  );
 
   @override
   Widget build(BuildContext context) {
-     PreferredSize appBar = PreferredSize(
-      preferredSize: const Size.fromHeight(56.5),
-      child: AppBar(
-        title: Container(
-          alignment: const Alignment(-0.2, -0.9),
-        ),
-        toolbarHeight: 56.5,
-        backgroundColor: backgroundColor,
-        shadowColor: Colors.transparent,
-      ),
-    );
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: appBar,
-        body: InternshipList(internshipWebsites),
+        body: InternshipCard(),
       ),
     );
+  }
+}
+
+class InternshipCard extends StatelessWidget {
+  final List<InternshipWebsite> internshipWebsites = [
+    InternshipWebsite("Internshala", "https://internshala.com/", "internshala_logo_url"),
+    InternshipWebsite("LinkedIn", "https://www.linkedin.com", "linkedin_logo_url"),
+    InternshipWebsite("Indeed", "https://www.indeed.com", "https://prod.statics.indeed.com/eml/assets/images/logo/indeed_logo_1200x630.png"),
+    // Add more internship websites here
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        for (var website in internshipWebsites)
+          GestureDetector(
+            onTap: () {
+              launchURL(website.url);
+            },
+            child: Container(
+              margin: const EdgeInsets.all(20), // Add margin between list items
+              padding: const EdgeInsets.all(50.0),
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 223, 223, 223),
+                borderRadius: BorderRadius.circular(20),
+                border: Border.all(
+                  width: 2,
+                  color: Colors.black,
+                ),
+              ),
+              child: Text(
+                website.name,
+                style: GoogleFonts.manrope(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+      ],
+    );
+  }
+
+  void launchURL(String url) async {
+    if (await canLaunchUrlString(url)) {
+      await launchUrlString(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
 
 class InternshipWebsite {
   final String name;
   final String url;
+  final String logoUrl;
 
-  InternshipWebsite(this.name, this.url);
-}
-
-class InternshipList extends StatelessWidget {
-  final List<InternshipWebsite> internshipWebsites;
-
-  InternshipList(this.internshipWebsites);
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: internshipWebsites.length,
-      itemBuilder: (BuildContext context, int index) {
-        return ListTile(
-          title: Text(internshipWebsites[index].name),
-          onTap: () {
-            launchURL(Uri.parse('internshipWebsites[index]'));
-          },
-        );
-      },
-    );
-  }
-
-  void launchURL(Uri url) async {
-    if (await canLaunchUrl(url)) {
-      await launchUrl(url);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
+  InternshipWebsite(this.name, this.url, this.logoUrl);
 }
