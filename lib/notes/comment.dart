c327f6b38b0a2149a5f982d91273da55655d2df8
RJ-04 <riyanshjain2004@gmail.com>
2023-08-13 14:27:08 +0530

    final_1

--- lib/notes/comment.dart
+++ lib/notes/comment.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:notex/notes/notes_storage.dart';
+import 'package:notex/utils/comment_bar.dart';
 import 'package:notex/utils/constants.dart';
 
 class Comments extends StatefulWidget {
@@ -52,15 +55,77 @@ class _CommentsState extends State<Comments> {
 
     return Scaffold(
       appBar: appBar,
-      // body: StreamBuilder(
-      //   stream: FirebaseFirestore.instance
-      //       .collection('pdf')
-      //       .doc(widget.snap)
-      //       .collection('comments')
-      //       .snapshots(),
-      //   builder: (BuildContext context,
-      //       AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {},
-      // ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('pdf')
+            .doc(widget.snap['docid'])
+            .collection('comments')
+            .orderBy('date', descending: true)
+            .snapshots(),
+        builder: (BuildContext context,
+            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          return ListView.builder(
+            itemCount: (snapshot.data! as dynamic).docs.length,
+            itemBuilder: (context, index) {
+              return CommentBar(
+                  snap: (snapshot.data! as dynamic).docs[index].data());
+            },
+          );
+        },
+      ),
+      bottomNavigationBar: SafeArea(
+        child: Container(
+          height: kBottomNavigationBarHeight,
+          margin:
+              EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+          padding: const EdgeInsets.only(left: 16, right: 8),
+          child: Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 16, right: 8),
+                  child: TextField(
+                    controller: _commentController,
+                    autocorrect: true,
+                    decoration: const InputDecoration(
+                      hintText: "Comment",
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 45),
+                child: ElevatedButton(
+                  style: const ButtonStyle(
+                      backgroundColor: MaterialStatePropertyAll(
+                          Color.fromARGB(255, 223, 223, 223))),
+                  onPressed: () async {
+                    await postComment(
+                        _commentController.text.trim(), widget.snap['docid']);
+                    setState(() {
+                      _commentController.text = "";
+                    });
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      //   color: Colors.grey,
+                      // border: Border.all(width: 1)
+                    ),
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                    child: const Text(
+                      "Post",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
