bcba8adcbd886501c961348591f94fde1a743704
OmarMah <36341168+OmarMah@users.noreply.github.com>
2023-11-25 11:51:14 +0200

    made changes to ui of the explore notes page

--- lib/utils/pdf_box.dart
+++ lib/utils/pdf_box.dart
@@ -64,176 +64,139 @@ class _PdfBoxState extends State<PdfBox> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 05),
-      child: Container(
-        padding: const EdgeInsets.all(20),
-        decoration: BoxDecoration(
-          border: Border.all(width: 2),
-          color: const Color.fromARGB(255, 233, 233, 233),
-          borderRadius: const BorderRadius.all(Radius.circular(20)),
-        ),
-        child: Column(
-          children: [
-            // subject
+      padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+      child: Card(
+        elevation: 2,
+        child: Container(
+          padding: const EdgeInsets.fromLTRB(20, 15, 25, 5),
+          decoration: BoxDecoration(
+            border: Border.all(width: 0),
+            color: const Color.fromARGB(255, 246, 246, 246),
+            borderRadius: const BorderRadius.all(Radius.circular(5)),
+          ),
+          child: Column(
+            children: [
+              // subject
 
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                widget.snap['subject'],
-                style: GoogleFonts.manrope(
-                  fontSize: 30,
-                  fontWeight: FontWeight.w600,
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  widget.snap['subject'],
+                  style: GoogleFonts.manrope(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 5),
+              const SizedBox(height: 1),
 
-            // author
+              // author
 
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                "~${widget.snap['author']}",
-                style: GoogleFonts.manrope(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w200,
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "~${widget.snap['author']}",
+                  style: GoogleFonts.manrope(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w200,
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 25),
+              const SizedBox(height: 10),
 
-            Row(
-              children: [
-                // tag
+              Row(
+                children: [
+                  // tag
 
-                Container(
-                  alignment: Alignment.centerLeft,
-                  width: 90,
-                  height: 45,
-                  decoration: BoxDecoration(
-                    color: backgroundColor,
-                    border: Border.all(width: 1.5),
-                    borderRadius: const BorderRadius.all(Radius.circular(25)),
-                  ),
-                  child: Center(
-                    child: Text(
-                      widget.snap['tags'],
-                      style: GoogleFonts.manrope(
-                        fontSize: 20,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  ),
-                ),
-
-                // semester
-
-                Padding(
-                  padding: const EdgeInsets.only(left: 105),
-                  child: Text(
-                    "Semester ${widget.snap['semester']}",
-                    style: GoogleFonts.manrope(
-                      fontSize: 25,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 30),
-
-            // buttons
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                // view pdf
-
-                GestureDetector(
-                  onTap: () async {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => PdfView(
-                          url: widget.snap['pdfurl'],
-                          snap: widget.snap,
-                        ),
-                      ),
-                    );
-                  },
-                  child: Container(
+                  Container(
                     alignment: Alignment.centerLeft,
-                    width: 110,
-                    height: 45,
+                    width: 60,
+                    height: 30,
                     decoration: BoxDecoration(
-                      color: Colors.white,
+                      color: backgroundColor,
                       border: Border.all(width: 1.5),
-                      borderRadius: const BorderRadius.all(Radius.circular(25)),
+                      borderRadius: const BorderRadius.all(Radius.circular(3)),
                     ),
                     child: Center(
                       child: Text(
-                        "View PDF",
+                        widget.snap['tags'],
                         style: GoogleFonts.manrope(
-                          fontSize: 15,
-                          fontWeight: FontWeight.w400,
+                          fontSize: 12,
+                          fontWeight: FontWeight.w500,
                         ),
                       ),
                     ),
                   ),
-                ),
 
-                const SizedBox(width: 16),
+                  const SizedBox(width: 10),
 
-                // icons
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    IconButton(
-                      onPressed: share,
-                      icon: Icon(MdiIcons.send),
+                  // semester
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    width: 60,
+                    height: 30,
+                    decoration: BoxDecoration(
+                      color: backgroundColor,
+                      border: Border.all(width: 1.5),
+                      borderRadius: const BorderRadius.all(Radius.circular(3)),
                     ),
-                    IconButton(
-                      onPressed: () {
+                    child: Center(
+                      child: Text(
+                        'SEM: ${widget.snap['semester']}',
+                        style: GoogleFonts.manrope(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+
+              // buttons
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                      onPressed: () async {
                         Navigator.push(
                           context,
                           MaterialPageRoute(
-                            builder: (context) => Comments(snap: widget.snap),
+                            builder: (context) => PdfView(
+                              url: widget.snap['pdfurl'],
+                              snap: widget.snap,
+                            ),
                           ),
                         );
                       },
-                      icon: const Icon(FontAwesomeIcons.comment),
-                    ),
-                    IconButton(
-                      onPressed: () async {
-                        bool status = await Permission.storage.isDenied;
+                      icon: const Icon(FontAwesomeIcons.solidEye)),
+                  IconButton(
+                    onPressed: share,
+                    icon: Icon(MdiIcons.send),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Comments(snap: widget.snap),
+                        ),
+                      );
+                    },
+                    icon: const Icon(FontAwesomeIcons.comment),
+                  ),
+                  IconButton(
+                    onPressed: () async {
+                      bool status = await Permission.storage.isDenied;
 
-                        if (status) {
-                          await Permission.storage.request();
-                          bool granted =
-                              await Permission.storage.request().isGranted;
+                      if (status) {
+                        await Permission.storage.request();
+                        bool granted =
+                            await Permission.storage.request().isGranted;
 
-                          if (granted) {
-                            // ignore: use_build_context_synchronously
-                            showCupertinoDialog(
-                              context: context,
-                              builder: (context) {
-                                return const Align(
-                                  alignment: Alignment(0, -0.75),
-                                  child: SizedBox(
-                                    height: 5,
-                                    child: LinearProgressIndicator(
-                                      backgroundColor: Colors.transparent,
-                                      color: Colors.cyan,
-                                    ),
-                                  ),
-                                );
-                              },
-                            );
-
-                            await downloadPdf();
-                          }
-                        } else {
+                        if (granted) {
                           // ignore: use_build_context_synchronously
                           showCupertinoDialog(
                             context: context,
@@ -253,35 +216,54 @@ class _PdfBoxState extends State<PdfBox> {
 
                           await downloadPdf();
                         }
-                      },
-                      icon: const Icon(FontAwesomeIcons.download),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        IconButton(
-                          onPressed: () {
-                            setState(() {
-                              _liked = !_liked;
-                            });
-                            likePost(
-                              widget.snap['docid'],
-                              _liked,
-                              widget.snap['likes'],
+                      } else {
+                        // ignore: use_build_context_synchronously
+                        showCupertinoDialog(
+                          context: context,
+                          builder: (context) {
+                            return const Align(
+                              alignment: Alignment(0, -0.75),
+                              child: SizedBox(
+                                height: 5,
+                                child: LinearProgressIndicator(
+                                  backgroundColor: Colors.transparent,
+                                  color: Colors.cyan,
+                                ),
+                              ),
                             );
                           },
-                          icon: _liked
-                              ? const Icon(FontAwesomeIcons.solidThumbsUp)
-                              : const Icon(FontAwesomeIcons.thumbsUp),
-                        ),
-                        Text(widget.snap['likes'].toString())
-                      ],
-                    ),
-                  ],
-                ),
-              ],
-            )
-          ],
+                        );
+
+                        await downloadPdf();
+                      }
+                    },
+                    icon: const Icon(FontAwesomeIcons.download),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          setState(() {
+                            _liked = !_liked;
+                          });
+                          likePost(
+                            widget.snap['docid'],
+                            _liked,
+                            widget.snap['likes'],
+                          );
+                        },
+                        icon: _liked
+                            ? const Icon(FontAwesomeIcons.solidThumbsUp)
+                            : const Icon(FontAwesomeIcons.thumbsUp),
+                      ),
+                      Text(widget.snap['likes'].toString())
+                    ],
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
